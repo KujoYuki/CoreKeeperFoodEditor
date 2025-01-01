6b10a0c8a823e11e52aef3b90075eab70b168d2e
KujoYuki <ls15081j@gmail.com>
2025-01-02 01:29:40 +0900

    wip 解析処理の修正
    レシピと数が合わない（2158/2775）

--- CKFoodMakerTest/AnalyzeTest.cs
+++ CKFoodMakerTest/AnalyzeTest.cs
@@ -38,10 +38,20 @@ namespace CKFoodMakerTest
             List<int> rareMaterials = Enumerable.Range(8100, 11).Append(9733).ToList();
 
             // 全ての食材と料理の表示名を取得
-            Dictionary<int, string> allFoodMaterials = StaticResource.AllCookedBaseCategories
-                .Concat(StaticResource.AllFoodMaterials)
+            var foodMaterials = StaticResource.AllFoodMaterials
                 .Concat(StaticResource.ObsoleteFoodMaterials)
-                .ToDictionary(item => item.Info.objectID, item => item.DisplayName);
+                .Select(item => (objectID: item.Info.objectID, DisplayName: item.DisplayName))
+                .ToList();
+
+            var allFoodMaterials = StaticResource.AllCookedBaseCategories
+                .Select(item =>
+                {
+                    return new[] { item.Info.objectID, item.Info.objectID + (int)CookRarity.Rare, item.Info.objectID + (int)CookRarity.Epic }
+                    .Select(id => (objectID: id, DisplayName: item.DisplayName));
+                })
+                .SelectMany(food => food).ToList()
+                .Concat(foodMaterials)
+                .ToDictionary();
 
             // 全ての発見済みアイテムからレシピのみを抽出し、表示名を含めてcsv形式のレコード変換
             List<string> discoveredAllRecipe = _saveData["discoveredObjects2"]!.AsArray()
@@ -69,9 +79,13 @@ namespace CKFoodMakerTest
                 {
                     // csv用レコードに変換
                     Form1.ReverseCalcurateVariation(r.variation, out int materialA, out int materialB);
-                    return string.Join(',', materialA, materialB, r.objectID, allFoodMaterials[materialA], allFoodMaterials[materialB], allFoodMaterials[r.objectID]);
+                    return (materialA, materialB, r.objectID, materialNameA:allFoodMaterials[materialA], materialNameB:allFoodMaterials[materialB], recepeCategoryName:allFoodMaterials[r.objectID]);
                 })
+                .OrderBy(r => r.materialA)
+                .ThenBy(r => r.materialB)
+                .Select(r => string.Join(',', r.materialA, r.materialB, r.objectID, r.materialNameA, r.materialNameB, r.recepeCategoryName))
                 .ToList();
+
 
             var sb = new StringBuilder();
             sb.AppendLine("食材A_Id,食材B_Id,料理_Id,食材A_名前,食材B_名前,料理名");
